519ca3f33ea0bd26281211c8305c1cf5d4e92706
Jade Allen Cook <jadeallencook@Jades-Macbook-Pro.local>
2018-03-30 14:21:39 -0600

    🔓 Added login admin panel

--- dev/src/app/app.component.ts
+++ dev/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    
+  }
 }
--- dev/src/environments/environment.ts
+++ dev/src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    config: {
+      apiKey: "AIzaSyAWMXKQMNmdzq1BQzPuSjfeVqqIddH-XfE",
+      authDomain: "yoga-ear.firebaseapp.com",
+      databaseURL: "https://yoga-ear.firebaseio.com",
+      projectId: "yoga-ear",
+      storageBucket: "",
+      messagingSenderId: "715545961598"
+    }
+  }
 };
