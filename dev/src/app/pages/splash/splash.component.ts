1ad90e5e8ca0ff7ba6dfa23d956a1f6413bf1612
Jade Allen Cook <jadeallencook@Jades-Macbook-Pro.local>
2018-04-05 00:45:47 -0600

    🎥 Started video player page

--- dev/src/app/pages/splash/splash.component.ts
+++ dev/src/app/pages/splash/splash.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-splash',
@@ -7,7 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
 
-  constructor() { }
+  playing: any = '';
+  music: any = '';
+  featured: string = '';
+  speaker: string = '';
+  speakers: Array<string> = [];
+
+  constructor(public sanitizer: DomSanitizer) {
+    this.getVideo(false);
+    this.getMusic(false);
+  }
+
+  getMusic(autoplay) {
+    firebase.database().ref('/music').once('value').then((snapshot) => {
+      const genres = Object.keys(snapshot.val());
+      const genre = genres[Math.floor(Math.random() * genres.length)];
+      const uids = Object.keys(snapshot.val()[genre]);
+      const uid = uids[Math.floor(Math.random() * uids.length)];
+      const id = this.getYouTubeVideoId(snapshot.val()[genre][uid].video);
+      console.log(id);
+      if (autoplay) autoplay = '&autoplay=true';
+      else autoplay = '&autoplay=false';
+      const url = 'https://www.youtube.com/embed/' + id + '?rel=0&showinfo=0' + autoplay;
+      this.music = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    });
+  }
+
+  getVideo(autoplay) {
+    firebase.database().ref('/lectures').once('value').then((snapshot) => {
+      this.speakers = Object.keys(snapshot.val());
+      const randomSpeaker = this.speakers[Math.floor(Math.random() * this.speakers.length)];
+      for (let speaker of environment.speakers) {
+        if (speaker.slug === randomSpeaker) this.speaker = speaker.name;
+      }
+      const uids = Object.keys(snapshot.val()[randomSpeaker]);
+      const uid = uids[Math.floor(Math.random() * uids.length)];
+      this.featured = this.getYouTubeVideoId(snapshot.val()[randomSpeaker][uid].video);
+      if (autoplay) autoplay = '&autoplay=true';
+      else autoplay = '&autoplay=false';
+      const url = 'https://www.youtube.com/embed/' + this.featured + '?rel=0&showinfo=0' + autoplay;
+      this.playing = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    });
+  }
+
+  getYouTubeVideoId(url) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match && match[7].length == 11) ? match[7] : false;
+  }
 
   ngOnInit() {
   }
