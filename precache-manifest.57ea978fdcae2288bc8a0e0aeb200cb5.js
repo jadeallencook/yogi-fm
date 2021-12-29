ad1c4916bd126a8188884d0e56a165e056ff88b1
Jade Allen Cook <jadeallencook@gmail.com>
2021-12-29 14:59:04 -0800

    Updates

---
+++ precache-manifest.57ea978fdcae2288bc8a0e0aeb200cb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e8d7414bf9b07ae973",
+    "url": "/Yogi-FM/build/static/css/main.30ae7c60.chunk.css"
+  },
+  {
+    "revision": "26e8d7414bf9b07ae973",
+    "url": "/Yogi-FM/build/static/js/main.26e8d741.chunk.js"
+  },
+  {
+    "revision": "b97096add126fd12b6da",
+    "url": "/Yogi-FM/build/static/js/1.b97096ad.chunk.js"
+  },
+  {
+    "revision": "5160589c4ae89134d8d2",
+    "url": "/Yogi-FM/build/static/js/runtime~main.5160589c.js"
+  },
+  {
+    "revision": "626829cefce271459a62f318a65328f6",
+    "url": "/Yogi-FM/build/index.html"
+  }
+];
